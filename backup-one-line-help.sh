e55687e12e4569f3d7cdc14c90b9268e643794cb
Andreas Langenhagen <andreas.langenhagen@here.com>
2018-08-28 15:45:33 +0200

    Clarify the backupe-one-line-help.sh docstring

--- backup-one-line-help.sh
+++ backup-one-line-help.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Backs up the one-line-help file into an archive file sorts the lines
+# Appends the one-line-help file contents to an archive file,
+# sorts the lines there
 # and removes duplicate entries from that archive file.
 #
 # It is supposed to run as a cronjob, thus the paths to the programs are fully qualified.
 #
-# author: langenhagen
-# version 170726
+# author: andreasl
+# version 180828
 
 input_file="/Users/langenha/stuff/one-line-helps/one-line-help-langenha-4demlangenha.txt"
 backup_file="/Users/langenha/personal/Dev/Zeugs/one-line-helps/one-line-help-langenha-4demlangenha-archive.txt"
