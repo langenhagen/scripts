5fcf8dfecc63b24b87342e2b83c5e46565a3206f
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2018-11-09 13:58:03 +0100

    Add git-pul-rebase-all-git-subdirectories.sh
    This script can call `git pull -rebase` on a given or the current
    directory path.

---
+++ git-pull-rebase-all-git-subdirectories.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Given a directory, will call git pull rebase on all its subdirectories recursively to or
+# by the given number of directory levels.
+#
+# Usage: $0 [-d|--depth <number>] [<path>]
+#
+# Examples:
+#   $0                      # updates all git repos in the cwd and its subdirs
+#   $0 -d 1                 # updates all git repos immediately under the cwd
+#   $0 -d 3 path/to/dir     # updates all git repos in the given path up to 3 levels deep
+#   $0 path/to/dir          # updates all git repos in the given dir and its subdirs
+#
+# author: andreasl
+# version: 18-11-09
+
+use_maxdepth=false
+while [ $# -gt 1 ] ; do
+    key="$1"
+    case ${key} in
+    -d|--depth)
+        use_maxdepth=true
+        depth="${2}"
+        shift # past argument
+        ;;
+    *) # unknown option
+        ;;
+    esac
+    shift # past argument or value
+done
+
+if [ $# -gt 0 ]; then
+    search_dir="${1}"
+else
+    search_dir='.'
+fi
+
+if [ ${use_maxdepth} == true ]; then
+    find "${search_dir}" -maxdepth ${depth} -type d -iname "*.git" -execdir \
+        bash -c 'printf "\033[1m${PWD}\033[0m\n"; git pull --rebase; ' '{}' \;
+else
+    find "${search_dir}" -type d -iname "*.git" -execdir \
+        bash -c 'printf "\033[1m${PWD}\033[0m\n"; git pull --rebase; ' '{}' \;
+fi
