0f16413d247c856e36077cdcce388e95b2690b2b
Andreas Langenhagen <andreas@langenhagen.cc>
2018-12-15 16:53:49 +0100

    Rename snippet.sh to snippet-query.sh

--- snippet-query.sh
+++ snippet-query.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
 # Given one or several input args,
-# If there are any lines that contain matches for each input arg and the string '#snippet',
+# If there are any lines that contain matches for each input arg and the string '#snippet' in a
+# certain hardcoded file,
 # print all these lines to the output and write the first line to the clipboard.
 #
 # author: andreasl
-# version: 18-12-12
+# version: 18-12-13
 
-data_file="${HOME}/Work/2018-CeleraOne/day-notes.md"
-results=$(grep -i '#snippet' "${data_file}")
+file="${HOME}/Work/2018-CeleraOne/day-notes.md"
+results=$(grep -i '#snippet' "${file}")
 for searchterm in "${@}" ; do
     results="$(printf '%s' "${results}" | grep -i "${searchterm}")"
 done
