af735c22681d03d7637c02b65b0a661d19c86f1c
Andreas Langenhagen <andreas@langenhagen.cc>
2018-12-15 16:54:11 +0100

    Add suckless-..-query.sh scripts

---
+++ suckless-snippet-query.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Opens a dmenu which prompts for any number of whitespace-separated search terms and suggests
+# old queries.
+# Greps iteratively in a certain hard coded file for lines that contain all of the given search
+# terms and the special term '#snippet'.
+# From these lines, the trimmed parts before the '#snippet' are presented in a second dmenu pass
+# from which the user can select one.
+# The raw selected item will be written to the system clipboard.
+# This is a neat way of querying and retrieving snippets into any desktop environment.
+#
+# TODO review the docs.
+#
+# author: andreasl
+# version: 18-12-14
+
+query_history_file="${HOME}/.ssq_history"
+historic_queries="$(tac ${query_history_file})"
+
+query="$(printf "${historic_queries}" | dmenu -i -l 5 -p "snippet query?:" )"
+if [ $? != 0 ] ; then
+    exit 1
+fi
+
+file="${HOME}/Work/2018-CeleraOne/day-notes.md"
+results=$(grep -i '#snippet' "${file}")
+for searchterm in ${query} ; do
+    results="$(printf '%s' "${results}" | grep -i "${searchterm}")"
+done
+
+if [ -z "${results}" ] ; then
+    exit 1
+fi
+
+selected_result="$(printf '%s\n' "${results[@]}" | sed 's/[ \t]*#snippet.*$//' | dmenu -i -p "select:" -l 30)"
+if [ -z "${selected_result}" ] ; then
+    exit 1
+fi
+
+sed -i "/${query}/d" "${query_history_file}"
+echo "${query}" >> "${query_history_file}"
+
+printf '%s' "${selected_result}" | head -1 | xclip -i -f -selection primary | xclip -i -selection clipboard
---
+++ suckless-tricks-query.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Opens a dmenu which prompts for any number of whitespace-separated search terms and suggests
+# old queries.
+# Greps iteratively the tricks.txt file for lines that contain all of the given search terms.
+# These lines are presented in a second dmenu pass from which the user can select one.
+# The raw selected item will be written to the system clipboard.
+# This is a neat way of querying and retrieving snippets from the tricks.md file.
+#
+# TODO review the docs.
+#
+# author: andreasl
+# version: 18-12-14
+
+query_history_file="${HOME}/.stq_history"
+historic_queries="$(tac ${query_history_file})"
+
+query="$(printf "${historic_queries}" | dmenu -i -l 5 -p "tricks query?:" )"
+if [ $? != 0 ] ; then
+    exit 1
+fi
+
+file="${HOME}/Dev/Zeugs/tricks.txt"
+results=$(cat "${file}")
+for searchterm in ${query} ; do
+    results="$(printf '%s' "${results}" | grep -i "${searchterm}")"
+done
+
+if [ -z "${results}" ] ; then
+    exit 1
+fi
+
+selected_result="$(printf '%s\n' "${results[@]}" | dmenu -i -p "select:" -l 30)"
+if [ -z "${selected_result}" ] ; then
+    exit 1
+fi
+
+sed -i "/${query}/d" "${query_history_file}"
+echo "${query}" >> "${query_history_file}"
+
+printf '%s' "${selected_result}" | head -1 | xclip -i -f -selection primary | xclip -i -selection clipboard
