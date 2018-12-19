3daad040af2af99bd13e6b1c556053cb4801feb1
Andreas Langenhagen <andreas@langenhagen.cc>
2018-12-19 23:50:50 +0100

    Update add-ubuntu-key-shortcut.py
    It should be complete and final now.

--- add-ubuntu-key-shortcut.py
+++ add-ubuntu-key-shortcut.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 #
 # Add a Ubuntu global schema_and_key shortcut via command line.
+# Remap commands that already exist but share the same attribute 'name '.
 #
 # Usage:
 #   python3 $0 <command name> <command> <schema_and_key-kombination>
@@ -13,45 +14,58 @@
 # based on:
 # https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
 #
-#
-# TODO: remove a command that already has the given key-combination.
-#
 # author: andreasl
-# version: 18-12-01
+# version: 18-12-17
 
 import ast
 import subprocess
 import sys
 
-def get_list_from_cmd_output(cmd):
-    """Get the list-output of a bash command that looks as a python list.
-    In case the list was empty, remove the annotation hints '@as'.
-    """
-    list_as_string = subprocess.check_output(["/bin/bash", "-c", cmd]).decode("utf-8").lstrip("@as")
-    return ast.literal_eval(list_as_string)
+
+def exec_bash_cmd(cmd):
+    return subprocess.check_output(["/bin/bash", "-c", cmd]).decode("utf-8")
 
 
 schema_and_key = "org.gnome.settings-daemon.plugins.media-keys custom-keybindings"
-schema_and_key_slash_separated = "/" + schema_and_key.replace(" ", "/").replace(".", "/")
-already_installed_custom_shortcuts = get_list_from_cmd_output("gsettings get " + schema_and_key)
+cmd = "gsettings get " + schema_and_key
+shortcut_names = exec_bash_cmd(cmd).lstrip("@as").strip()
 
-# make sure the additional keybinding mention gets a new index
-index = 1
-while True:
-    new_entry = schema_and_key_slash_separated + "/" + "custom" + str(index) + "/"
-    if new_entry in already_installed_custom_shortcuts:
-        index = index + 1
-    else:
+# iterate over all existing shortcuts to see if one with the same name already exists and reuse that
+already_installed_shortcuts = ast.literal_eval(shortcut_names)
+entry = None
+for s in already_installed_shortcuts:
+
+    check_name_cmd = 'gsettings get ' + \
+        schema_and_key[:-1].replace(" ", ".") + ':' + s + ' name'
+    shortcut_name = exec_bash_cmd(check_name_cmd)
+
+    if shortcut_name.strip()[1:-1].lower() == sys.argv[1].lower():
+        entry = s
         break
 
-subkey = schema_and_key.replace(" ", ".")[:-1] + ":"
-already_installed_custom_shortcuts.append(new_entry)
-# to create the shortcut, set the name, command and shortcut schema_and_key
-cmd0 = 'gsettings set ' + schema_and_key + ' "' + str(already_installed_custom_shortcuts) + '"'
-cmd1 = 'gsettings set ' + subkey + new_entry + " name '" + sys.argv[1] + "'"
-cmd2 = 'gsettings set ' + subkey + new_entry + " command '" + sys.argv[2] + "'"
-cmd3 = 'gsettings set ' + subkey + new_entry + " binding '" + sys.argv[3] + "'"
 
-for cmd in [cmd0, cmd1, cmd2, cmd3]:
+# if no shortcut with the given name exists, create a new one
+schema_and_key_slash_separated = "/" + \
+    schema_and_key.replace(" ", "/").replace(".", "/")
+if not entry:
+    entry = schema_and_key_slash_separated + "/custom1/"
+    index = 2
+    while entry in already_installed_shortcuts:
+        entry = schema_and_key_slash_separated + "/custom" + str(index) + "/"
+        index += 1
+    already_installed_shortcuts.append(entry)
+    append_entry_to_shortcuts_cmd = 'gsettings set ' + \
+        schema_and_key + ' "' + str(already_installed_shortcuts) + '"'
+    exec_bash_cmd(append_entry_to_shortcuts_cmd)
+
+
+subkey_and_entry = schema_and_key[:-1].replace(" ", ".") + ":" + entry
+
+# to create the shortcut, set the name, command and shortcut schema_and_key
+set_name_cmd    = 'gsettings set ' + subkey_and_entry + " name '"    + sys.argv[1] + "'"
+set_command_cmd = 'gsettings set ' + subkey_and_entry + " command '" + sys.argv[2] + "'"
+set_binding_cmd = 'gsettings set ' + subkey_and_entry + " binding '" + sys.argv[3] + "'"
+
+for cmd in [set_name_cmd, set_command_cmd, set_binding_cmd]:
     print(" +  {}".format(cmd))
-    subprocess.call(["/bin/bash", "-c", cmd])
+    exec_bash_cmd(cmd)
