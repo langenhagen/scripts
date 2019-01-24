78f4e53b0f8130039704a435e1c415464e426930
Andreas Langenhagen <andreas@langenhagen.cc>
2019-01-24 23:02:14 +0100

    Refactor clean-diskspace.sh

--- clean-diskspace.sh
+++ clean-diskspace.sh
@@ -6,14 +6,16 @@
 #   --wholesome (optional): calls git `gq --aggressive` on all .git folders (indirectly) under "/".
 #
 # author: andreasl
-# version: 19-01-11
+# version: 19-01-19
 
 set -x;
 
+# Empty Trash
 dirsize=$(du -sh "~/.local/share/Trash/" 2>/dev/null);
 printf "\033[1mInfo: ~/.local/share/Trash has the following size: ${dirsize}\033[0m\n";
 rm -rfv "~/.local/share/Trash/*";
 
+# Gradle & related
 dirsize=$(du -sh ~/.gradle/caches 2>/dev/null);
 printf "\033[1mInfo: ~/.gradle/caches has the following size: ${dirsize}\033[0m\n";
 rm -rfv ~/.gradle/caches;
@@ -27,6 +29,9 @@ dirsize=$(du -sh ~/.m2/repository 2>/dev/null);
 printf "\033[1mInfo: ~/.m2/repository has the following size: ${dirsize}\033[0m\n";
 rm -rfv ~/.m2/repository;
 
+find ~ -iname '.gradle' -type d -exec rm -rf '{}' \;
+
+# Buildout
 dirsize=$(du -sh ~/.buildout/download-cache 2>/dev/null);
 printf "\033[1mInfo: ~/.buildout/download-cache has the following size: ${dirsize}\033[0m\n";
 rm -rfv ~/.buildout/download-cache;
@@ -34,8 +39,10 @@ dirsize=$(du -sh ~/.buildout/eggs 2>/dev/null);
 printf "\033[1mInfo: ~/.buildout/eggs has the following size: ${dirsize}\033[0m\n";
 rm -rfv ~/.buildout/eggs;
 
+# Brew
 command -v brew >/dev/null && brew cleanup -s;
 
+# Apt
 if [ "$(command -v apt)" ] ; then
     sudo du -sh /var/cache/apt/archives;
     sudo apt clean;
@@ -43,12 +50,16 @@ if [ "$(command -v apt)" ] ; then
     sudo apt autoremove;
 fi
 
-find ~ -iname '.gradle' -type d -exec rm -rf '{}' \;
-
+# Docker
 command -v docker >/dev/null && docker system prune --all --force;
 
+# Git
 find ~ -type d -name '.git' -exec bash -c "pushd '{}'; git gc; popd ;" \;
 if [[ "$1" == "--wholesome" ]]; then
    printf "\033[1mDo a  git gq --aggressive  on all git repos on the machine\033[0m\n";
    find / -type d -name '.git' -exec bash -c "pushd '{}'; sudo git gc --aggressive; popd ;" \;
 fi
+
+# Python
+find ~ -name "*.pyc" -delete;
+find ~ -name "*.pyo" -delete;
