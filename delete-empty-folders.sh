cd3485e052852a541912672d0d4c8a7bdfa13c66
Andreas Langenhagen <andreas@langenhagen.cc>
2019-04-04 01:22:38 +0200

    Refactor delete-empty-folders.sh

--- delete-empty-folders.sh
+++ delete-empty-folders.sh
@@ -3,24 +3,24 @@
 # Finds all empty subfolders in a hardcoded directory
 # and prompts the user whether to delete every individual empty folder,
 # and does accordingly.
-#
+# Ignores everything inside .git dirs.
 # author: andreasl
 
-root_folder='/Users/langenha/personal/Dev/Cpp'  # adjust this
+root_folder='/home/barn/Dev'  # adjust this
 
 directory_array=()
 while IFS=  read -r -d $'\0'; do
     directory_array+=("$REPLY")
 done < <(find "${root_folder}" -type d -print0)
 
-for ((i = 0; i < ${#directory_array[@]}; i++)) do
-    current_directory="${directory_array[$i]}"
-    is_empty=$(ls -A "${current_directory}")
+for dir in "${directory_array[@]}" ; do
+    if [[ "${dir}" =~ .*\.git/.* ]] ; then
+        continue
+    fi
+    is_empty=$(ls -A "${dir}")
     if [ -z "${is_empty}" ]; then
-        echo "${current_directory}"
+        echo "${dir}"
         read -r -e -n1 -p " Delete [y/n]?: " do_delete  # -r raw, i.e. don't mangle backslashes -e newline after input is read  -n1 capture 1 character, no ENTER needed
-        if [ "${do_delete}" == "y" ]; then
-            rm -rf "${current_directory}"
-        fi
+        [ "${do_delete}" == "y" ] && rm -rf "${dir}"
     fi
 done
