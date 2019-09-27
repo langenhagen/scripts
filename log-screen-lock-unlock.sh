fe8a55622853613a719798d34c7f3e8a671db158
Andreas Langenhagen <andreas@langenhagen.cc>
2019-09-28 01:21:02 +0200

    Add log-screen-lock-unlock.sh
    Add the script log-screen-lock-unlock.sh that may be able to log my
    computer-busy times as a service.

---
+++ log-screen-lock-unlock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Log when the screen is locked or unlocked on gnome 3.
+# Works with screen locking (e.g. super + l) and going to hibernation.
+#
+# based on:
+# https://unix.stackexchange.com/questions/28181/run-script-on-screen-lock-unlock
+
+log_file_path="${HOME}/Administrative/logs/logins-$(uname -n)-$(date '+%Y').csv"
+
+dbus-monitor --session "type='signal',interface='org.gnome.ScreenSaver'" |
+  while read x; do
+    case "$x" in
+      *"boolean true"*) printf "$(date),locked\n" >> "$log_file_path";;
+      *"boolean false"*) printf "$(date)\n,unlocked" >> "$log_file_path";;
+    esac
+  done
