bae69b0e70f571a1d0e9fbb8ce7b73f6d1c5c6b7
Andreas Langenhagen <andreas@langenhagen.cc>
2019-12-06 02:37:13 +0100

    Add grepselect.fish

---
+++ grepselect.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/fish
+# Select some of the recent output of grep -Hrn and open the according files in vim.
+#
+# author: andreasl
+# version: 19-12-06
+
+set command (history | head -1)
+set files (eval $command | fzf -m | cut -d: -f1 | uniq)
+printf '%s\n' -- $files
+vim -p $files
