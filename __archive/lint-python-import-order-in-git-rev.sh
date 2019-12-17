e099714ab9df2d18635c7945a5f68e58f2f76af6
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2019-12-17 12:01:18 +0100

    Add lint-python-import-order-in-git-rev.sh

---
+++ __archive/lint-python-import-order-in-git-rev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Import order is a tough one, since there exist some unforseeable exceptions to the general rule.
+# I could apply the import order however only to the c1 block. that would be a small gain.
+
+
+
+# isort needs real files, doesn't work with here-documents
+
+# always returns 0
+# isort --diff --future future --future __future__ --multi-line 3 --project c1 --section-default THIRDPARTY --trailing-comma **.py  # --multi-line: 3-vert-hanging
+
+# may return 1 in case of error
+# isort --check-only --future future --future __future__ --multi-line 3 --project c1 --section-default THIRDPARTY --trailing-comma **.py  # --multi-line: 3-vert-hanging
+
+
+
