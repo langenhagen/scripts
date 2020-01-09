7cf4522bcde8dec012206ce84b609cbbd3ae9074
Andreas Langenhagen <andreas@langenhagen.cc>
2020-01-09 20:56:32 +0100

    Refactor some template docstrings

--- templates/add-file-header.sh
+++ templates/add-file-header.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#
-# Adds or updates file headers for different file types.
+# Add or update file headers for different file types.
 # Ideally, file headers may be retrieved from files from version control that are read into a
 # variable, but they may as well be defined in this file.
 #
