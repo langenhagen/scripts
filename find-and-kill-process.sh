8218bd3ace5c37a19d9b45f38dda94e0b63ec072
Andreas Langenhagen <andreas.langenhagen@celeraone.com>
2020-04-14 16:07:47 +0200

    Add find-and-kill-process.sh

---
+++ find-and-kill-process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Grep for a given process name and interactively kill one of the results.
+#
+# author: andreasl
+
+mapfile -t processes <<< "$(ps aux | grep -i "$@")"
+for key in "${!processes[@]}"; do
+    echo "$((key + 1))    ${processes[$key]}";
+done
+
+read -e -p "Kill process with number: " key
+pid="$(printf "${processes[$((key - 1))]}\n" | awk '{print $2}')"
+kill $pid
