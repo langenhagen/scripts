e57e33119a63a3bc64b878c132ae0bd49fad552a
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-08-06 18:14:37 +0200

    Handle company gitla nicely in push_to_remotes.sh
    Also refactor the docstring accordingly and reformat some of the other
    code.

--- push_to_remotes.sh
+++ push_to_remotes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Push to all remotes of the given git repo and take special care for gerrit remotes.
+# Push to all remotes of the given git repo and take special care for gerrit and company gitlab
+# remotes.
 #
 # author: andreasl
 
@@ -11,9 +12,8 @@ for remote_and_url in "${remotes_and_urls[@]}"; do
 
     remote="$(printf -- '%s' "$remote_and_url" | awk '{print $1}')"
 
-    if [[ "$remote_and_url" == *"29418"* ]]; then
+    if [[ "$remote_and_url" == *'29418'* ]]; then
         # push to gerrit
-
         remote_and_branch="$(git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)")"
         remote_branch="${remote_and_branch/$remote\//}"
         if [ -n "$remote_branch" ]; then
@@ -24,10 +24,22 @@ for remote_and_url in "${remotes_and_urls[@]}"; do
 
         # open according gerrit page in browser
         printf "$push_output" \
-                | tee /dev/stderr \
-                | grep -m1 -o 'https://.*' \
-                | awk '{print $1}' \
-                | xargs xdg-open
+            | tee /dev/stderr \
+            | grep -m1 -o 'https://.*' \
+            | awk '{print $1}' \
+            | xargs xdg-open
+    elif [[ "$remote_and_url" == *"gitlab.bof.mm.local"* ]]; then
+        >&2 printf -- "$remote_and_url"
+        # push merge-request to gitlab
+        remote_branch="$(git log --oneline --format='%s' -n1 | sed -E 's/[^_a-zA-Z0-9-]+/-/g;s/^-+|-+$//g;s/./\L&/g')"
+
+        push_output="$(git push "$remote" HEAD:"$remote_branch" 2>&1)"
+        printf '%s' "$push_output"
+        if [[ "$push_output" == *' * [new branch] '* ]]; then
+            # open according gitlab page in browser
+            gitlab_url=$(grep 'remote:   http[s]*://' <<< "$push_output" | grep -o 'http.*$')
+            xdg-open "$gitlab_url"
+        fi
     else
         # default
         git push "$remote"
