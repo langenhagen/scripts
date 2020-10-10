341508139c9d1a2e970df0cf168fe01ec5f6e7b1
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-11 01:12:16 +0200

    Change ui of fuzzy-git-log.sh

--- fuzzy-git-log.sh
+++ fuzzy-git-log.sh
@@ -4,10 +4,12 @@
 # author: andreasl
 
 git log \
-    --format="%C(yellow)%h %C(red)%d% %C(bold green)%ai %C(reset)%s %C(blue)<%an> %C(reset)" \
+    --format='%C(yellow)%h %C(red)%d% %C(bold green)%ai %C(reset)%s %C(blue)<%an> %C(reset)' \
     --color=always | \
     fzf \
         --ansi \
+        --multi \
         --no-sort \
-        --preview 'printf "%s" {} | cut -d" " -f1 | xargs -I@ sh -c "git show -p --stat --color=always @"' | \
+        --preview 'printf "%s" {} | cut -d" " -f1 | xargs -I@ sh -c "git show -p --stat --color=always @"' \
+        --reverse | \
     sed -e 's/^[[:space:]]*//;s/[[:space:]]*$//'
