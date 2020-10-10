5dca3c9b3b68272c7d2cb7df44925f5ebf58cdaa
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2020-10-11 00:55:53 +0200

    Simplify and improve fuzzy-git-log.sh

--- fuzzy-git-log.sh
+++ fuzzy-git-log.sh
@@ -4,9 +4,10 @@
 # author: andreasl
 
 git log \
-    --format="%C(yellow)%h%C(red)%d%C(reset) %C(bold green)%ai %C(reset)%s %C(blue)<%an> %C(reset)" \
+    --format="%C(yellow)%h %C(red)%d% %C(bold green)%ai %C(reset)%s %C(blue)<%an> %C(reset)" \
     --color=always | \
     fzf \
         --ansi \
         --no-sort \
-        --preview 'printf "%s" {} | grep -o "[a-f0-9]\{7\}" | head -1 | xargs -I@ sh -c "git show -p --stat --color=always @"'
+        --preview 'printf "%s" {} | cut -d" " -f1 | xargs -I@ sh -c "git show -p --stat --color=always @"' | \
+    sed -e 's/^[[:space:]]*//;s/[[:space:]]*$//'
