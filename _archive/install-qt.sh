90dfff71ba4eca5c7f1f5518d442c3bbeb6b620d
Andreas Langenhagen <andreas.langenhagen@momox.biz>
2021-02-18 22:40:54 +0100

    Add install-qt.sh
    You never know when this one might be helpful :)

---
+++ _archive/install-qt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Download and install QT and its prerequisites.
+sudo apt update;
+sudo apt upgrade -y;
+
+sudo apt-get install build-essential libgl1-mesa-dev;  # basic QT requirements
+
+sudo apt install -y qtcreator;
+sudo apt install -y qt5-default;  # set QT 5 as the default
+
+sudo apt install -y qt5-doc:  # Qt 5 API Documentation.
+sudo apt install -y qtbase5-examples;  # Qt Base 5 examples.
+sudo apt install -y qtbase5-doc-html;  # HTML documentation for the Qt 5 Base libraries
