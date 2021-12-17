0918107d6b25ec51a84ed6106efb9f5c649778e2
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2021-12-17 17:37:20 +0100

    Add get-deeper-commit.sh

---
+++ get-deeper-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# From 2 given commits, print the commit that is deeper in the ancestry line.
+#
+# Usage:
+#
+#  get-deeper-commit.sh aaaaaa  bbbbbb      # prints `aaaaaa``, assuming aaaaaa is the older commit
+#  get-deeper-commit.sh HEAD~4  HEAD~5      # prints `HEAD~5`
+#
+# author: andreasl
+
+ancestry_path="$(git rev-list --ancestry-path "${1}..${2}")" || exit
+
+[ -n "$ancestry_path" ] && echo "$1" || echo "$2"
