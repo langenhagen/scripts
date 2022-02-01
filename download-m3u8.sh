22f2d4a1634e8dc4653a2b826d06673f573286f0
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2022-02-01 22:10:06 +0100

    Add download-m3u8.sh

---
+++ download-m3u8.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Download an m3u8 file as a *.mkv file.
+#
+# Usage:
+#
+#   download-m3u8.sh <URL> <OUT-FILE>
+#
+# author: andreasl
+
+m3u8_url="$1"
+out_file="${2:-stream.mkv}"
+
+time ffmpeg -i "$m3u8_url" -c copy "$out_file"
