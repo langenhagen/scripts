cf28f305f31b7705f4a901fd5a8b6be508fd0a28
Andreas Langenhagen <andreas.langenhagen@micropsi-industries.com>
2023-04-08 23:20:57 +0200

    Use `m4v` files as default in `download-m3u8.sh`

--- download-m3u8.sh
+++ download-m3u8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Download an m3u8 file as a *.mkv file.
+# Download an m3u8 file as a *.m4v file.
 #
 # Usage:
 #
@@ -8,6 +8,9 @@
 # author: andreasl
 
 m3u8_url="$1"
-out_file="${2-stream.mkv}"
+out_file="${2-stream.m4v}"
+
+
+[ "${out_file%.*}" == "$out_file" ] && out_file="${out_file}.m4v"
 
 time ffmpeg -i "$m3u8_url" -c copy "$out_file"
